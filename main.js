41cdf70fcd764f5472b6ea78c5216be71cb9a11c
Aaron Dixson <88754367+AarondevPortfoloio@users.noreply.github.com>
2023-04-15 15:39:31 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+ 
+ let randomNum = Math.floor(Math.random() * 10);
+ function randomInspiration(){
+
+
+        switch(randomNum){
+
+        case 0:
+            console.log("You are amazing!");
+            break;
+        case 1:
+            console.log("You are the greatest");
+            break;
+        case 2:
+            console.log("You are super kind");
+            break;
+        case 3:
+            console.log("You are  very intelligent");
+            break;
+        case 4:
+            console.log("You are extremely brave");
+            break;
+        case 5:
+            console.log("You are the nicest person.");
+            break;
+        case 6:
+            console.log("You will have a great day today!");
+            break;
+        case 7:
+            console.log("Keep on trying never give up!");
+            break;
+        case 8:
+            console.log("You can do anything you put your mind too!");
+            break;
+        case 9:
+            console.log("Have a great day !");
+            break;
+        default:
+            console.log("If there is a will there is a way!");
+        }
+
+        
+
+ }
+
+
+ console.log(randomInspiration());
